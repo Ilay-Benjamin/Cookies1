f1192e11d25ce1c5fa666e0897e6d597f43c4225
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-05-03 13:20:41 +0300

    Creating Server-side

--- server.php
+++ server.php
@@ -1,8 +1,60 @@
 <?php
 
-session_start();
 
-$_SESSION['username'] = "admin-Ilay";
-$_SESSION['logged_in'] = true;
+    if (isset($_GET['action'])) {
+        match ($params['action']) {
+            'enter' => enter(),
+            'hey' => hey(),
+            default => die('Invalid action'),
+        };
+    }
+
+    if (isset($_POST['action'])) {
+        match ($params['action']) {
+            'logout' => logout(),
+            'login' => login(),
+            default => die('Invalid action'),
+        };
+    }
+
+
+    function hey() {
+        $usernameAsString = $_SESSION['username'] ?? 'Guest';
+        $message = "Heyyy " . $usernameAsString . ' (From the server)!';
+        $data = new stdClass();
+        $data->message = $message;
+        $json = json_encode($data);
+        echo $json;
+    }
+
+    function enter() {
+        session_start();
+        $_SESSION['username'] = "";
+        $_SESSION['logged_in'] = false;
+        $data = new stdClass();
+        $data->message="Welcome to Ilay's website!";
+        $json = json_encode($data);
+        echo $json;
+    }
+
+    function logout() {
+        session_destroy();
+        header('Location: /');
+    }
+
+    function login() {
+        if (isset($_POST['username']) && strlen($_POST['username']) > 0) {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['logged_in'] = true;
+            header('Location: /');
+        } else {
+            echo 'Invalid credentials';
+        }
+    }
+
+
+    $_SESSION['username'] = "admin-Ilay";
+    $_SESSION['logged_in'] = true;
+
 
 ?>
