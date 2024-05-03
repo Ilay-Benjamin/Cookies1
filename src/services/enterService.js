27da184356a69943f1cfb8b092dab6f9b28c82bb
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-05-04 02:08:29 +0300

    Creating Server-side

---
+++ src/services/enterService.js
@@ -0,0 +1,6 @@
+export function enter() {
+    var response = await fetch("https://ilaychecks.online/Apps/app5/server/app/server.php?action=enter");
+    var data = await response.json();
+    var message = data.message;
+    addMessage(message);
+}
