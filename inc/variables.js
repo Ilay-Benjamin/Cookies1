111283710191d4d6237b8c593ae717289773c69a
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-05-04 02:54:23 +0300

    Creating Server-side

---
+++ inc/variables.js
@@ -0,0 +1,47 @@
+
+// Details Div
+var details;
+
+// Output Div
+var output;
+
+// Messages Div
+var messagesDiv;
+
+// Name Div
+var nameDiv;
+
+// Note Div
+var noteDiv;
+
+// Hey Server Button
+var heyServerButton;
+
+// Contact Input
+var contactInput;
+
+// Contact Button
+var contactButton;
+
+// Name Input
+var nameInput;
+
+// Name Button
+var nameButton;
+
+// Note Input
+var noteInput;
+
+// Note Button
+var noteButton;
+
+// Cookie Manager Object
+var cManager;
+
+// Name Section Object
+var nameSect;
+
+// Note Section Object
+var noteSect;
+
+
