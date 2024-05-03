93b4590126eff0636a4f19f28442bec242872187
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-05-03 15:19:12 +0300

    Creating Server-side

---
+++ basics.js
@@ -0,0 +1,76 @@
+    
+// Details Div
+var details = document.getElementById("details");
+
+// Output Div
+var output = document.getElementById("output");
+
+// Messages Div
+var messagesDiv = document.getElementById("messages_div");
+
+// Name Div
+var nameDiv = document.getElementById("name_div");
+
+// Note Div
+var noteDiv = document.getElementById("note_div");
+
+// Hey Server Button
+var heyServerButton = document.getElementById("hey_server_button");
+
+// Contact Input
+var contactInput = document.getElementById("c_input");
+
+// Contact Button
+var contactButton = document.getElementById("c_button");
+
+// Name Input
+var nameInput = document.getElementById("name_input");
+
+// Name Button
+var nameButton = document.getElementById("name_button");
+
+// Note Input
+var noteInput = document.getElementById("note_input");
+
+// Note Button
+var noteButton = document.getElementById("note_button");
+
+
+
+function loadHTMLElements() {
+    // Details Div
+    details = document.getElementById("details");
+
+    // Output Div
+    output = document.getElementById("output");
+
+    // Messages Div
+    messagesDiv = document.getElementById("messages_div");
+
+    // Name Div
+    nameDiv = document.getElementById("name_div");
+
+    // Note Div
+    noteDiv = document.getElementById("note_div");
+
+    // Hey Server Button
+    heyServerButton = document.getElementById("hey_server_button");
+
+    // Contact Input
+    contactInput = document.getElementById("c_input");
+
+    // Contact Button
+    contactButton = document.getElementById("c_button");
+
+    // Name Input
+    nameInput = document.getElementById("name_input");
+
+    // Name Button
+    nameButton = document.getElementById("name_button");
+
+    // Note Input
+    noteInput = document.getElementById("note_input");
+
+    // Note Button
+    noteButton = document.getElementById("note_button");
+}
