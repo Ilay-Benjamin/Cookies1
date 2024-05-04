0fa2c08777d5c1fd800fdf58d3d6543f68c4b31c
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-05-04 04:38:58 +0300

    Creating Server-side

--- src/models/section.js
+++ src/models/section.js
@@ -152,6 +152,7 @@ export class Section {
     };
     var isSignedIn = cManager.isSignedIn();
     var ability = !isSignedIn;
+    console.log("< SectionFactory.NAME_SECTION() > ability-param: " + ability);
     return new Section(index, label, elements, ability);
   }
 
@@ -165,6 +166,7 @@ export class Section {
     };
     var isSignedIn = cManager.isSignedIn();
     var ability = isSignedIn;
+    console.log("< SectionFactory.NOTE_SECTION() > ability-param: " + ability);
     return new Section(index, label, elements, ability);
   }
 
